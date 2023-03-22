5d684fa2a7bf00f3973325efc79131e349d8842e
zendroo <gabriel_zendron@estudante.sesisenai.org.br>
2023-03-22 12:23:45 -0300

    alterando codigo6

--- app/src/main/java/com/example/calculadora9/errado.java
+++ app/src/main/java/com/example/calculadora9/errado.java
@@ -10,5 +10,6 @@ public class errado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_errado);
+        getSupportActionBar().hide();
     }
 }
