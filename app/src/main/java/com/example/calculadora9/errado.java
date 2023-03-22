28e0e5c0be8483c3bbdff83cc27924c844a80506
zendroo <gabriel_zendron@estudante.sesisenai.org.br>
2023-03-22 11:40:07 -0300

    add tela errado

---
+++ app/src/main/java/com/example/calculadora9/errado.java
@@ -0,0 +1,14 @@
+package com.example.calculadora9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class errado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_errado);
+    }
+}
